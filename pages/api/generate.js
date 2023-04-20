46327a99609aeaf5b06fedccfaa5b4c2c81dd517
Dongzheng Wu <dwuu0018@student.monash.edu>
2023-04-20 12:38:20 +1000

    Added recording feature - all the conversation will be recorded in conversation.txt under root folder. Integrated preConfig to improve the accuracy of test converting between Android UIAutomator and Selenium Web UI test.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,11 +31,11 @@ export default async function (req, res) {
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: generatePrompt(code, dest) }],
+      messages: [{role: "assistant", content: preConfig()}, { role: "user", content: generatePrompt(code, dest) }],
       temperature: 0
-    });
+    })
     res.status(200).json({ result: completion.data.choices[0].message.content });
-    console.log(response["data"]["choices"][0]["message"]["content"])
+    recordConversation(code, dest, completion.data.choices[0].message.content);
 
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
@@ -53,6 +53,10 @@ export default async function (req, res) {
   }
 }
 
+function preConfig(){
+  return "learn the code conversion between android UI automator code and javascript using selenum and assert below: \nmDevice.findObject(new UiSelector().text(\"TEXT_ON_COMPONENTS\"));	<-> 	driver.findElement(By.linkText(\"TEXT_ON_COMPONENTS\"))mDevice.findObject(new UiSelector().className(\"android.widget.EditText\"));	<-> 	driver.findElement(By.tagName(\"input\"))mDevice.findObject(new UiSelector().className(\"android.widget.Button\"));	<-> 	driver.findElement(By.tagName(\"button\"))mDevice.pressEnter();	<->	[INPUT_ELEMENT].sendKeys(\"'\ue007'\");"
+}
+
 function generatePrompt(code, dest) {
 
   if (dest == "") {
@@ -60,4 +64,15 @@ function generatePrompt(code, dest) {
   }
 
   return `convert following code to ${dest} \n ${code}`
+}
+
+/** write contents to files */
+var fs = require("fs");
+function recordConversation(code, dest, ans){
+  fs.appendFile('conversation.txt', "===== START OF CONVERSATION =====\n User:\n" + generatePrompt(code, dest)+"\n\n"+"ChatGPT:\n"+ans+"\n\n\n===== END OF CONVERSATION =====", (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+  console.log("appended!");
+  fs.close();
 }
